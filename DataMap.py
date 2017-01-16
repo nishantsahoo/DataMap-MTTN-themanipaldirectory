802de4448a671aa590582e6dc79f4a04a8e2e97a
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-01-17 00:36:56 +0530

    A fix to scrape the updated version of the website.

--- DataMap.py
+++ DataMap.py
@@ -14,7 +14,7 @@ i = 1
 for each in table_list:
     thead = each.find('thead').text.encode('utf-8').decode('ascii', 'ignore')
     print str(i) + '. ' + thead,
-    tBody = each.findChildren('tbody')
-    for tr in tBody:
+    tbody = each.findChildren('tbody')
+    for tr in tbody:
         print tr.text.encode('utf-8').decode('ascii', 'ignore')
     i += 1
