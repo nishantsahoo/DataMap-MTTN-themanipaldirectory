e1d58072cc833389e7caf468ccee8bf9b7b09143
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-01-18 06:57:13 +0530

    Need to extract tbody data for each table

--- DataMap.py
+++ DataMap.py
@@ -1,7 +1,7 @@
 import urllib2
 import sys
 from bs4 import BeautifulSoup
-sys.stdout = open('mttn_data_map.txt','w')
+sys.stdout = open('mttn_data_map_testing.txt','w')
 opener = urllib2.build_opener()
 opener.add_headers=[('User-agent','Mozilla/5.0')]
 url = "http://manipalthetalk.org/guides/the-manipal-directory"
@@ -11,11 +11,11 @@ print soup.find('h1', attrs={'class':'post-title entry-title'}).text
 mDirectory = soup.find('div', attrs={'id':'mdirectory'})
 table_list = mDirectory.findAll('table')
 i = 1
+k = 0
 for each in table_list:
     thead = each.find('thead').text.encode('utf-8').decode('ascii', 'ignore')
-    print str(i) + '. ' + thead,
+    print str(i) + '. ' + thead
     tBody = each.findChildren('tbody')
-    for tr in tBody:
-        print tr.text.encode('utf-8').decode('ascii', 'ignore'),
-    print
+    print tBody
     i += 1
+
