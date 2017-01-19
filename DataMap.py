9102cc96538da5f9cff99d233bd497ce105913e0
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-01-19 09:52:18 +0530

    Testing commit

--- DataMap.py
+++ DataMap.py
@@ -1,14 +1,14 @@
 import urllib2
 import sys
 from bs4 import BeautifulSoup
-sys.stdout = open('mttn_data_map.txt','w')
+sys.stdout = open('mttn_final_database.txt','w')
 opener = urllib2.build_opener()
 opener.add_headers=[('User-agent','Mozilla/5.0')]
 url = "http://manipalthetalk.org/guides/the-manipal-directory"
 ourUrl = opener.open(url).read()
 soup = BeautifulSoup(ourUrl, 'html.parser')
-print soup.find('h1', attrs={'class':'post-title entry-title'}).text
-print
+# print soup.find('h1', attrs={'class':'post-title entry-title'}).text
+# print
 mDirectory = soup.find('div', attrs={'id':'mdirectory'})
 table_list = mDirectory.findAll('table')
 i = 1
@@ -16,7 +16,7 @@ k = 0
 for each in table_list:
     thead = each.find('thead').text.encode('utf-8').decode('ascii', 'ignore')
     print str(i) + '. ' + thead
-    print
+    # print
     tRows = each.findAll('tr')
     for tr in tRows:
         tdList = tr.findAll('td')
@@ -27,5 +27,5 @@ for each in table_list:
                 else:
                     print td.text.encode('utf-8').decode('ascii', 'ignore')
                 k += 1
-        print
+        # print
     i += 1
