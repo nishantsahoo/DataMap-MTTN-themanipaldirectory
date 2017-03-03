a9e07030d6a3aa944c649d5c623eed2284fcdd94
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-03-03 12:42:25 +0530

    Working script.

--- DataMap.py
+++ DataMap.py
@@ -1,7 +1,7 @@
 import urllib2
 import sys
 from bs4 import BeautifulSoup
-sys.stdout = open('mttn_data_map_testing.txt', 'w')
+sys.stdout = open('mttn_final_database.txt', 'w')
 opener = urllib2.build_opener()
 opener.add_headers = [('User-agent', 'Mozilla/5.0')]
 url = "http://manipalthetalk.org/guides/the-manipal-directory"
@@ -13,8 +13,6 @@ soup = BeautifulSoup(ourUrl, 'html.parser')
 wrapper = soup.find('div', attrs={'id': 'wrapper'})
 main_inner_group = wrapper.find('div', attrs={'class': 'main-inner group'})
 inner = main_inner_group.find('div', attrs={'class': 'entry-inner'})
-print inner
-'''
 mDirectory = inner.find('div', attrs={'id': 'mdirectory'})
 table_list = mDirectory.findAll('table')
 k, i = 0, 1
@@ -33,4 +31,3 @@ for each in table_list:
                 k += 1
             # print
     i += 1
-'''
