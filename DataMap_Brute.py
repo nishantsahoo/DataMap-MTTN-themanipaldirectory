cbb15d176c015ee536d4971c712a8b29b9dda119
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-11-03 15:06:41 +0530

    Update

--- DataMap_Brute.py
+++ DataMap_Brute.py
@@ -1,7 +1,7 @@
 import urllib2
 import sys
 from bs4 import BeautifulSoup
-sys.stdout = open('mttn_data_map_brute.txt','w')
+sys.stdout = open('mttn_data_map_brute_new.txt','w')
 opener = urllib2.build_opener()
 opener.add_headers=[('User-agent','Mozilla/5.0')]
 url = "http://manipalthetalk.org/guides/the-manipal-directory"
